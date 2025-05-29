887daeb06859568cc4637ce7c3de7f8cf54d2027
Новиков Артемий <temanov0530@gmail.com>
2025-05-29 23:21:21 +0500

    sqlite3 -> postgres

--- src/database/database.py
+++ src/database/database.py
@@ -2,10 +2,13 @@
 from sqlalchemy import create_engine
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
+import os
 
 
-DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = sqlalchemy.orm.declarative_base()
 
