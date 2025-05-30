aa680df605a3cecc38a72e5d72d0ca9643c4b9de
Новиков Артемий <temanov0530@gmail.com>
2025-05-30 12:41:12 +0500

    change params

--- src/database/database.py
+++ src/database/database.py
@@ -7,7 +7,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20, pool_timeout=30)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = sqlalchemy.orm.declarative_base()
